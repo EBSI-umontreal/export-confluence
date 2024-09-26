adbd989cace798bdb5dea95a276f19634c8100a1
arnauddalayer <36932970+arnauddalayer@users.noreply.github.com>
2024-09-26 08:40:54 -0400

    20240926
    Mise à jour ANSICON (la version 1.84 causait un problème avec Defender)

--- src/go.bat
+++ src/go.bat
@@ -4,7 +4,7 @@ exit
 :native
 
 set JAVA_HOME=%~dp0vendor\jdk8u265-b01
-SET PATH=%PATH%;%~dp0;%~dp0vendor\phantomjs-2.1.1-windows\bin;%~dp0vendor\casperjs-1.1.4-1\bin;%~dp0vendor\apache-ant-1.10.4\bin;%~dp0vendor\ansicon184\x64;%~dp0vendor\calibre-portable-3.46\Calibre;
+SET PATH=%PATH%;%~dp0;%~dp0vendor\phantomjs-2.1.1-windows\bin;%~dp0vendor\casperjs-1.1.4-1\bin;%~dp0vendor\apache-ant-1.10.4\bin;%~dp0vendor\ansicon189\x64;%~dp0vendor\calibre-portable-3.46\Calibre;
 ansicon -p
 SET CLASSPATH=%CLASSPATH%;.;%~dp0vendor\SaxonHE9-8-0-12J\saxon9he.jar;
 
