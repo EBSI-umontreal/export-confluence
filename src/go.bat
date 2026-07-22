1d2e35f6465d860a19e5ba4c4c8f33595312317f
Arnaud d'Alayer <arnaud.dalayer@umontreal.ca>
2026-07-22 16:33:56 -0400

    20260722
    - Correction du pied de page des EPUB pour guides 2026-2027
    - Nouvelle feuille de style basé sur le guide d'image de marque de l'UdeM
    - Mise à jour de l'environnement (Calibre, Ant, JDK)

--- src/go.bat
+++ src/go.bat
@@ -3,8 +3,8 @@ IF "%PROCESSOR_ARCHITEW6432%"=="" GOTO native
 exit
 :native
 
-set JAVA_HOME=%~dp0vendor\jdk8u265-b01
-SET PATH=%PATH%;%~dp0;%~dp0vendor\phantomjs-2.1.1-windows\bin;%~dp0vendor\casperjs-1.1.4-1\bin;%~dp0vendor\apache-ant-1.10.4\bin;%~dp0vendor\ansicon189\x64;%~dp0vendor\calibre-portable-3.46\Calibre;
+set JAVA_HOME=%~dp0vendor\jdk-25.0.3+9
+SET PATH=%PATH%;%~dp0;%~dp0vendor\phantomjs-2.1.1-windows\bin;%~dp0vendor\casperjs-1.1.4-1\bin;%~dp0vendor\apache-ant-1.10.17\bin;%~dp0vendor\ansicon189\x64;%~dp0vendor\calibre-portable-8.00\Calibre;
 ansicon -p
 SET CLASSPATH=%CLASSPATH%;.;%~dp0vendor\SaxonHE9-8-0-12J\saxon9he.jar;
 
